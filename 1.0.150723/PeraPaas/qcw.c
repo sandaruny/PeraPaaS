d92a523d930b74bdced7c5431fb4570fd8f6509f
sandaruny <sandaru.ny@gmail.com>
2015-08-01 13:16:34 +0530

    Included UDPClient Project to call services

--- 1.0.150723/PeraPaas/qcw.c
+++ 1.0.150723/PeraPaas/qcw.c
@@ -14,17 +14,46 @@
 #include <sys/time.h>
 #include <stdlib.h>
 #include "ppmsg.h"
+#include <unistd.h>
+#include <errno.h>
+#include <pthread.h>
+#include <arpa/inet.h>
+#include<unistd.h>
+
+#include <signal.h>
 
 /*
  * 
  */
 
-static int pool_size = 0, web_req_count = 0;
+int listener_d;
+
+void* request_balancer();
+void error(char *msg);
+
+void handle_shutdown(int sig);
+void register_sigkill_fn();
+void register_sigkill_fn(){
+
+    if (signal(SIGUSR1, handle_shutdown) == SIG_ERR)
+        printf("\ncan't catch SIGUSR1\n");
+    if (signal(SIGKILL, handle_shutdown) == SIG_ERR)
+        printf("\ncan't catch SIGKILL\n");
+    if (signal(SIGSTOP, handle_shutdown) == SIG_ERR)
+        printf("\ncan't catch SIGSTOP\n");
+    
+}
+
+
+
+
+static int pool_size = 0, *web_req_count = 0;
 
 int main(int argc, char** argv) {
 
     char addr_command[] = "mystock/findstock/eurousd";
 
+    
     struct message msg;
     struct message msg_queue[3];
 
@@ -48,12 +77,19 @@ int main(int argc, char** argv) {
     char mesg[1000];
 
 
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    servaddr.sin_family = AF_INET;
+    sockfd = socket(PF_INET, SOCK_STREAM, 0);
+
+
+    servaddr.sin_family = PF_INET;
     servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // htonl(INADDR_ANY); //inet_addr("127.0.0.1");
     servaddr.sin_port = htons(32000);
 
-    bind(sockfd, (struct sockaddr *) &servaddr, sizeof (servaddr));
+
+
+    if (bind(sockfd, (struct sockaddr *) &servaddr, sizeof (servaddr)) == -1)
+        error("Can't bind the port");
+
+
     len = sizeof (cliaddr);
 
     time_t rawtime;
@@ -69,21 +105,32 @@ int main(int argc, char** argv) {
     struct timeval stop, start;
 
 
-    pid_t timer_pid = fork();
-
-    if (timer_pid == 0) {
-        while (1) {
-            sleep(1);
-            printf("Req Count: %d\n", web_req_count);
-        }
+    pthread_t t0;
+    if (pthread_create(&t0, NULL, request_balancer, NULL) == -1) {
+        printf("Error in pthread for request balance");
     }
 
+    if (listen(sockfd, 10) == -1)
+        error("Can't listen");
+
     while (1) {
+        struct sockaddr_storage client_addr;
+        unsigned int address_size = sizeof (client_addr);
+
+        int connect_d = accept(sockfd, (struct sockaddr *) &client_addr, &address_size);
 
 
-        n = recvfrom(sockfd, mesg, 1000, 0, (struct
+        if (connect_d == -1) {
+            printf("Cannot open a socket");
+            break;
+        }
+        web_req_count++;
+
+        n = recvfrom(connect_d, mesg, 1000, 0, (struct
                 sockaddr*) &cliaddr, &len);
+        
 
+        printf("Recieved: %d\n", web_req_count);
         int pid = fork();
 
         if (pid == 0) {
@@ -92,12 +139,11 @@ int main(int argc, char** argv) {
                 banner = asctime(timeinfo);
                 time(&rawtime);
                 timeinfo = localtime(&rawtime);
-
-                sendto(sockfd, banner, strlen(banner), 0, (struct sockaddr
-                        *) &cliaddr, sizeof (cliaddr));
+                sleep(1);
+                send(connect_d, banner, strlen(banner), 0);
                 int line_cnt = 0;
                 printf("Recieved \n");
-                sleep(1);
+
             }
         }
 
@@ -105,3 +151,23 @@ int main(int argc, char** argv) {
     return (EXIT_SUCCESS);
 }
 
+void* request_balancer() {
+
+    while (1) {
+        sleep(1);
+        printf("Req Count: %d\n", web_req_count);
+    }
+
+}
+
+void error(char *msg) {
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(1);
+}
+
+void handle_shutdown(int sig) {
+    if (listener_d)
+        close(listener_d);
+    fprintf(stderr, "Bye!\n");
+    exit(0);
+}
---
+++ TestingTools/UDPClient/client.c
@@ -0,0 +1,58 @@
+/* Sample UDP client */
+//#include <winsock2.h>
+
+
+#include <string.h>
+
+
+#include  <sys/types.h>
+//#include <time.h>
+#include <sys/time.h>
+#include <stdlib.h>
+
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LEN 80
+
+int main(int argc, char**argv) {
+    int sockfd, n;
+    struct sockaddr_in servaddr;
+    char sendline[] = "Hello UDP server! This is UDP client";
+    char recvline[1000];
+    char scantxt[100];
+
+
+    sockfd = socket(PF_INET, SOCK_STREAM, 0); // socket(AF_INET, SOCK_DGRAM, 0);
+    bzero(&servaddr, sizeof (servaddr));
+    memset(&servaddr, 0, sizeof (servaddr));
+    servaddr.sin_family = PF_INET;
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servaddr.sin_port = htons(32000);
+
+    connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+    send(sockfd, sendline, strlen(sendline), 0);
+
+
+    while (1) {
+
+        n = recv(sockfd, recvline, 1000, 0);
+        if(n < 1){
+            break;
+        }
+        recvline[n] = '\0';
+        printf("Time  %d: %s\n", n,recvline);
+        //  sleep(1);
+
+    }
+
+
+    recvline[n] = 0;
+
+
+    return 0;
+}
